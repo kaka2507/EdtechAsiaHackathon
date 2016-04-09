9cb044f229b5053bd7461629c4ad7b4bbacb11f9
Huy Pham <huyphamthanh8290@gmail.com>
2016-04-09 19:50:40 +0700

    Playground: show song list

--- Android/Muzikator/app/src/main/java/act/muzikator/activity/PlaygroundActivity.java
+++ Android/Muzikator/app/src/main/java/act/muzikator/activity/PlaygroundActivity.java
@@ -1,15 +1,82 @@
 package act.muzikator.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
 
 import act.muzikator.R;
+import butterknife.Bind;
+import butterknife.OnClick;
 
-public class PlaygroundActivity extends AppCompatActivity {
+public class PlaygroundActivity extends BaseActivity {
+    @Bind(R.id.songsButton)       Button     songsButton;
+    @Bind(R.id.collectionButton)  Button     collectionButton;
+    @Bind(R.id.songWrapper)       ScrollView songWrapper;
+    @Bind(R.id.collectionWrapper) ScrollView collectionWrapper;
+
+    private boolean isSongsSelected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playground);
+
+        showSongTab();
+    }
+
+    @OnClick(R.id.songsButton)
+    public void showSongTab() {
+        if (!isSongsSelected) {
+            isSongsSelected = true;
+
+            songWrapper.setVisibility(View.VISIBLE);
+            songsButton.setBackgroundResource(R.drawable.ic_song_bt_selected);
+
+            collectionWrapper.setVisibility(View.INVISIBLE);
+            collectionButton.setBackgroundResource(R.drawable.ic_collection_bt);
+        }
+    }
+
+    @OnClick(R.id.collectionButton)
+    public void showCollectionTab() {
+        if (isSongsSelected) {
+            isSongsSelected = false;
+
+            songWrapper.setVisibility(View.INVISIBLE);
+            songsButton.setBackgroundResource(R.drawable.ic_song_bt);
+
+            collectionWrapper.setVisibility(View.VISIBLE);
+            collectionButton.setBackgroundResource(R.drawable.ic_collection_bt_selected);
+        }
+    }
+
+    @OnClick(R.id.song1)
+    public void song1Selected() {
+        startSinging();
+    }
+
+    @OnClick(R.id.song2)
+    public void song2Selected() {
+        startSinging();
+    }
+
+    @OnClick(R.id.song3)
+    public void song3Selected() {
+        startSinging();
+    }
+
+    @OnClick(R.id.song4)
+    public void song4Selected() {
+        startSinging();
+    }
+
+    @OnClick(R.id.song5)
+    public void song5Selected() {
+        startSinging();
+    }
+
+    private void startSinging() {
+
     }
 }
