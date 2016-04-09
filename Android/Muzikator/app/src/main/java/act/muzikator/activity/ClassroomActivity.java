7568c5c3e7028618c52f3f74b54d6bf136ec2e1c
Huy Pham <huyphamthanh8290@gmail.com>
2016-04-09 18:45:22 +0700

    Classroom: show tutor list & go to video call when click on a tutor

--- Android/Muzikator/app/src/main/java/act/muzikator/activity/ClassroomActivity.java
+++ Android/Muzikator/app/src/main/java/act/muzikator/activity/ClassroomActivity.java
@@ -1,19 +1,77 @@
 package act.muzikator.activity;
 
 import android.os.Bundle;
-import android.widget.ToggleButton;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
 
 import act.muzikator.R;
 import butterknife.Bind;
+import butterknife.OnClick;
 
 public class ClassroomActivity extends BaseActivity {
-    @Bind(R.id.popularButton) ToggleButton popularButton;
-    @Bind(R.id.historyButton) ToggleButton historyButton;
+    @Bind(R.id.popularButton)  Button     popularButton;
+    @Bind(R.id.historyButton)  Button     historyButton;
+    @Bind(R.id.popularWrapper) ScrollView popularWrapper;
+    @Bind(R.id.historyWrapper) ScrollView historyWrapper;
+
+    private boolean isPopularSelected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classroom);
+
+        showPopularTab();
     }
 
+    @OnClick(R.id.popularButton)
+    public void showPopularTab() {
+        if (!isPopularSelected) {
+            isPopularSelected = true;
+
+            popularWrapper.setVisibility(View.VISIBLE);
+            popularButton.setBackgroundResource(R.drawable.ic_class_tab_popular_selected);
+
+            historyWrapper.setVisibility(View.INVISIBLE);
+            historyButton.setBackgroundResource(R.drawable.ic_class_tab_history);
+        }
+    }
+
+    @OnClick(R.id.historyButton)
+    public void showHistoryTab() {
+        if (isPopularSelected) {
+            isPopularSelected = false;
+
+            popularWrapper.setVisibility(View.INVISIBLE);
+            popularButton.setBackgroundResource(R.drawable.ic_class_tab_popular);
+
+            historyWrapper.setVisibility(View.VISIBLE);
+            historyButton.setBackgroundResource(R.drawable.ic_class_tab_history_selected);
+        }
+    }
+
+    @OnClick(R.id.tutor1)
+    public void tutor1Selected() {
+        videoCallWithTutor();
+    }
+
+    @OnClick(R.id.tutor2)
+    public void tutor2Selected() {
+        videoCallWithTutor();
+    }
+
+    @OnClick(R.id.tutor3)
+    public void tutor3Selected() {
+        videoCallWithTutor();
+    }
+
+    @OnClick(R.id.tutor4)
+    public void tutor4Selected() {
+        videoCallWithTutor();
+    }
+
+    private void videoCallWithTutor() {
+
+    }
 }
